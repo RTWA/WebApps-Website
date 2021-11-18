a3a38b1845f516396733ba2117201c406f8545a6
Rob Trehy <robert.trehy@outlook.com>
2021-11-18 09:53:15 +0000

    Added Cookie and Privacy Policies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,63 +1,36 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-    purge: [
-        './dist/*.html'
-    ],
+    purge: {
+        content: [
+            './dist/index.html',
+            './dist/privacy.html',
+            './dist/cookies.html'
+        ],
+        options: {
+            keyframes: true,
+            fontFace: true,
+        },
+
+    },
     darkMode: 'media', // false or 'media' or 'class'
     theme: {
-        // colors: {
-        //   'indigo': colors.indigo,
-        //   'gray': colors.gray,
-        //   'white': colors.white,
-        //   'black': colors.black,
-        // },
-        // extend: {
-        //     // colors: {
-        //     //   'blue-gray': colors.blueGray,
-        //     //   'light-blue': colors.sky,
-        //     //   'orange': colors.orange,
-        //     //   'lime': colors.lime,
-        //     //   'fuchsia': colors.fuchsia
-        //     // },
-        //     // zIndex: {
-        //     //     '-10': '-10',
-        //     //     2: 2,
-        //     //     3: 3,
-        //     // },
-        //     // width: {
-        //     //     'xl': '36rem',
-        //     // },
-        //     // minHeight: {
-        //     //     '64': '16rem',
-        //     //     'screen-75': "75vh"
-        //     // },
-        //     // fontSize: {
-        //     //     55: "55rem",
-        //     // },
-        // },
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.coolGray,
+            indigo: colors.indigo,
+            red: colors.red,
+            blue: colors.blue,
+            green: colors.green,
+        }
     },
     variants: {
         extend: {
-            backgroundColor: ["checked"],
-            borderColor: ["checked"],
-            inset: ["checked"],
             display: ['dark'],
         },
     },
-    // variants: [
-    //     "responsive",
-    //     "group-hover",
-    //     "focus-within",
-    //     "first",
-    //     "last",
-    //     "odd",
-    //     "even",
-    //     "hover",
-    //     "focus",
-    //     "active",
-    //     "visited",
-    //     "disabled",
-    // ],
 };
 
